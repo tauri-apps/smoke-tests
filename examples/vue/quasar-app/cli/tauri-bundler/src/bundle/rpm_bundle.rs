3796ec60ffd919bd04f8c7439da22ce15bc74192
Denjell <denjell@sfosc.org>
2020-02-12 13:44:12 +0100

    fix(structure): remove inception

--- examples/vue/quasar-app/cli/tauri-bundler/src/bundle/rpm_bundle.rs
+++
@@ -1,7 +0,0 @@
-use crate::Settings;
-
-use std::path::PathBuf;
-
-pub fn bundle_project(_settings: &Settings) -> crate::Result<Vec<PathBuf>> {
-  unimplemented!();
-}
--- examples/vue/quasar-app/cli/tauri.js/bin/tauri-info.js
+++
@@ -1,3 +0,0 @@
-const info = require('../dist/api/info')
-
-info()
